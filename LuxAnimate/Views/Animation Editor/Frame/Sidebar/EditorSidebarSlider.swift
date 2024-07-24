5a1500f1ce538db666a98405818c5b105271dc50
Adam Shin <adamshindev@gmail.com>
2024-07-24 17:59:40 -0600

    Slider flick

--- LuxAnimate/Views/Animation Editor/Frame/Sidebar/EditorSidebarSlider.swift
+++ LuxAnimate/Views/Animation Editor/Frame/Sidebar/EditorSidebarSlider.swift
@@ -5,10 +5,10 @@
 import UIKit
 
 private let hPadding: CGFloat = 12
-private let vPadding: CGFloat = 8
+private let vPadding: CGFloat = 10
 
 private let barWidth: CGFloat = 44
-private let barHeight: CGFloat = 150
+private let barHeight: CGFloat = 152
 
 private let thumbInset: CGFloat = 5
 private let thumbHeight: CGFloat = 16
@@ -19,13 +19,16 @@ private let selectedScale = (barWidth + 2) / barWidth
 private let thumbNormalColor = UIColor(white: 1, alpha: 0.7)
 private let thumbSelectedColor = UIColor.editorLabel
 
-private let dragRate: CGFloat = 0.5
+private let dragRate: CGFloat = 0.6
+
+private let flickVelocityThreshold: CGFloat = 500
+private let flickDecelDuration: TimeInterval = 0.2
+private let flickDistanceMultiplier: CGFloat = 0.0005
 
 protocol EditorSidebarSliderDelegate: AnyObject {
     
     func onBeginPress(_ v: EditorSidebarSlider)
     func onEndPress(_ v: EditorSidebarSlider)
-    
     func onChangeValue(_ v: EditorSidebarSlider)
     
 }
@@ -112,14 +115,14 @@ class EditorSidebarSlider: UIView {
     
     @objc private func onPress() {
         switch pressGesture.state {
-        case .began: 
+        case .began:
             showBeginPress()
             delegate?.onBeginPress(self)
             
-        case .changed: 
+        case .changed:
             break
             
-        default: 
+        default:
             showEndPress()
             delegate?.onEndPress(self)
         }
@@ -131,17 +134,32 @@ class EditorSidebarSlider: UIView {
             panGestureStartValue = internalValue
             
         case .changed:
-            guard let panGestureStartValue
-            else { return }
+            guard let panGestureStartValue else { return }
             
             let translation = panGesture.translation(in: self)
             
-            let thumbRangeSize = barHeight -
-                thumbHeight -
-                thumbInset * 2
+            let thumbRangeSize = barHeight
+                - thumbHeight
+                - thumbInset * 2
             
             let dv = -translation.y / thumbRangeSize * dragRate
             value = panGestureStartValue + dv
+            
+        case .ended:
+            panGestureStartValue = nil
+            
+            let velocity = panGesture.velocity(in: self)
+            
+            if abs(velocity.y) > flickVelocityThreshold {
+                let finalValue = clamp(
+                    value - velocity.y * flickDistanceMultiplier,
+                    min: 0, max: 1)
+                
+                UIView.animate(springDuration: flickDecelDuration) {
+                    self.value = finalValue
+                    layoutIfNeeded()
+                }
+            }
             
         default:
             panGestureStartValue = nil
@@ -188,7 +206,7 @@ extension EditorSidebarSlider: UIGestureRecognizerDelegate {
     
     func gestureRecognizer(
         _ gestureRecognizer: UIGestureRecognizer,
-        shouldRecognizeSimultaneouslyWith 
+        shouldRecognizeSimultaneouslyWith
         otherGestureRecognizer: UIGestureRecognizer
     ) -> Bool {
         return otherGestureRecognizer == pressGesture
