c8e9a72b1e6a24804e6a4887936a3f61ac990af1
Adam Shin <adamshindev@gmail.com>
2024-08-30 23:12:49 -0600

    Remove comments

--- LuxAnimate/Views/Test Editor/Frame Editor/TestFrameEditor.swift
+++ LuxAnimate/Views/Test Editor/Frame Editor/TestFrameEditor.swift
@@ -5,30 +5,6 @@
 import UIKit
 import Metal
 
-// Thought: Should the frame editor be recreated whenever
-// we change frames, layers, or tools? The frame editor
-// would then represent an editing session that pertains
-// to a single drawing, and a single tool.
-
-// This may simplify some things. I'll have to think about
-// where to reuse code, since all tools will share certain
-// logic. They all have to generate the frame scene, load
-// assets, render etc. This should probably live in the
-// frame editor, not the individual tool.
-
-// I like the idea of making a clean break whenever we
-// switch tools or drawings. Maybe when switching tools, we
-// should also reload the active drawing from disk. This
-// would help ensure everything stays in sync.
-
-// A benefit here is that we don't have to update the
-// existing frame editor when the selected tool changes.
-// We just create a fresh new frame editor. I think this
-// simplifies the logic.
-
-// When starting a new editor session, we should reuse
-// already-loaded assets from the previous session.
-
 private let sceneSize = PixelSize(1920, 1080)
 private let drawingSize = PixelSize(1000, 1000)
 
--- LuxAnimate/Views/Test Editor/TestEditorVC.swift
+++ LuxAnimate/Views/Test Editor/TestEditorVC.swift
@@ -4,53 +4,6 @@
 
 import UIKit
 
-// TODO: Figure out how all this should work?
-
-// Scene state and renderer should live here maybe.
-// As well as the main display link.
-
-// Workspace vc should keep track of workspace transform.
-
-// Should each tool exist as an object? Which installs
-// itself into the view hierarchy?
-
-// How far should the drawing editor logic be factored out?
-
-// The process will need to look like:
-
-// 1. Load the scene, including assets
-// 2. Begin a drawing session. Allocate canvas texture
-// 3. Instantiate the brush tool. Allocate memory, etc
-// 4. Respond to edits and tool changes.
-
-// The drawing editor logic shouldn't need to worry about
-// loading, or about the rest of the scene. It should
-// propagate updates back up to here, where we assemble
-// the scene data and render everything.
-
-// Maybe, when selecting a tool, we should:
-// 1. Update the UI. Install new view controllers for
-//    controls and gestures
-// 2. Create a new tool editor object which manages the
-//    internal logic of the tool
-// 3. Pass the view controllers to the tool, so it can
-//    hook itself up with delegates/references etc to
-//    handle gestures and other inputs
-
-// Second thought. Maybe the tool picker and tool control
-// UI should exist at a higher level from the drawing
-// editor itself. In the future, there will be a timeline
-// vc as well for changing frames. We want the control UI
-// to exist at a higher level instead of being recreated
-// whenever the selected frame changes.
-
-// Maybe the workspace view controller also lives at this
-// higher level, as well as the tool gesture recognizers.
-// Tool state UI would be managed at this level, and
-// internal tool state would also be managed inside the
-// drawing editor. The trick will be getting them to
-// communicate properly.
-
 class TestEditorVC: UIViewController {
     
     private let bodyView = TestEditorView()
